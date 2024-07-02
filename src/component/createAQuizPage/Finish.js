8beab43df6dfd08c648c4e71f449c7fcb524e5b7
Rishu Raj <92977357+Alpha-911@users.noreply.github.com>
2024-07-03 01:22:52 +0530

    Update Finish.js

--- src/component/createAQuizPage/Finish.js
+++ src/component/createAQuizPage/Finish.js
@@ -16,7 +16,7 @@ function Finish({setQuizCode, setQuizData, setSelectedOption, generatedCode, isL
                             <img src="https://raw.githubusercontent.com/Alpha-911/Brainer/a0b6c402c89c18c2a2e0badd893cea3614f3586d/public/tick-circle.svg" alt="tick-circle"/>
                             <h1>Quiz Successfully Created</h1>
                             <div className={'quiz-code'}>
-                                <img src="ticket.svg" alt=""/>
+                                <img src="https://raw.githubusercontent.com/Alpha-911/Brainer/8c3e889ab967f2f58bb974ea9e9df82de2c0f9a6/public/ticket.svg" alt=""/>
                                 <h1>{generatedCode}</h1>
                             </div>
                         </div>
